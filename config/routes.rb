c0540ef8d470f4dfa777f31b282af4f3dcf88182
Pawan Kumar <pwnsub2001@pec.edu>
2023-02-17 16:27:56 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
   resources :vehicles
+  root "vehicles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
