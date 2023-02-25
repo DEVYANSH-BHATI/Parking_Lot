c3435f54ea13ed334c2c77a7aa0e98086dc57e5a
Devyansh <devyansh.bhati333@pec.edu>
2023-02-25 22:44:55 +0530

    alert , routes,app.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   # get 'home/index'
   resources :vehicles
   root 'home#index'
-=======
-  # get 'home/index'
-  resources :vehicles
-  root "vehicles#index"
->>>>>>> 94123aba118a5c9d3bdd8108a019cea1ccaf1fdd
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
