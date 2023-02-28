ec9e75a146c2ff7ef6f0f2ced93aced0ac177626
Pawan Kumar <pwnsub2001@pec.edu>
2023-02-28 14:55:51 +0530

    charge view

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_115914) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_090949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "charges", force: :cascade do |t|
-    t.string "type"
+    t.string "vehicle_type"
     t.integer "min_charge"
     t.integer "min_hours"
     t.integer "extra_hour_charges"
--- test/controllers/charges_controller_test.rb
+++ test/controllers/charges_controller_test.rb
@@ -1,8 +1,48 @@
 require "test_helper"
 
 class ChargesControllerTest < ActionDispatch::IntegrationTest
-  test "should get charges" do
-    get charges_charges_url
+  setup do
+    @charge = charges(:one)
+  end
+
+  test "should get index" do
+    get charges_url
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_charge_url
+    assert_response :success
+  end
+
+  test "should create charge" do
+    assert_difference("Charge.count") do
+      post charges_url, params: { charge: { extra_hour_charges: @charge.extra_hour_charges, min_charge: @charge.min_charge, min_hours: @charge.min_hours, vehicle_type: @charge.vehicle_type } }
+    end
+
+    assert_redirected_to charge_url(Charge.last)
+  end
+
+  test "should show charge" do
+    get charge_url(@charge)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_charge_url(@charge)
+    assert_response :success
+  end
+
+  test "should update charge" do
+    patch charge_url(@charge), params: { charge: { extra_hour_charges: @charge.extra_hour_charges, min_charge: @charge.min_charge, min_hours: @charge.min_hours, vehicle_type: @charge.vehicle_type } }
+    assert_redirected_to charge_url(@charge)
+  end
+
+  test "should destroy charge" do
+    assert_difference("Charge.count", -1) do
+      delete charge_url(@charge)
+    end
+
+    assert_redirected_to charges_url
   end
 end
