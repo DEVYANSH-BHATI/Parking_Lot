2a8d6b173041c6756aef82075d177e5be2d0a0b6
Devyansh <devyansh.bhati333@pec.edu>
2023-03-12 21:26:45 +0530

    minor changes to vehicle model

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,6 +1,6 @@
 class Charge < ApplicationRecord
     # if chose to admin then render the charges view
-    has_many :vehicle
+    # has_many :vehicle
 
     # search
     def self.search(search)
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,11 +1,32 @@
 class Vehicle < ApplicationRecord
     # belongs_to :admin
-    has_many :charge
-    has_one :user
+    # belongs_to :charge
+    
 
     #validations
-    validates_presence_of :vehicle_type,:number,:in_time,:out_time,:user_id
-    validates :out_time ,comparison: {greater_than: :in_time}
+    validates_uniqueness_of :number
+    validates_presence_of :vehicle_type,:number,:owner
+    # validates :out_time ,comparison: {greater_than: :in_time}
+    validates :status ,:inclusion => %w[ parked left ]
+    # valid                                      {accepted |parked,left|}
+    # before_validation put intime function
+    before_validation  :put_intime_and_status , on: :create
+
+    def put_intime_and_status
+        pp"put intime"
+        self.in_time = DateTime.now
+        # self.out_time = DateTime.now + 10.days
+        self.status = 'parked'
+    end
+    # out tiem at press of button should trigger
+    def put_out_time
+
+    end
+
+    def vehicle_left
+        self.status = left
+    end
+
 
 
     # search
